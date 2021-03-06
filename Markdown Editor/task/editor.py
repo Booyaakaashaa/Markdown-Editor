7834c55086736675f458c71a6f7f3d1ebcbb25f3
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-07 00:36:54 +0530

    Markdown python

---
+++ Markdown Editor/task/editor.py
@@ -0,0 +1,7 @@
+print(r"""# The List of Albums
+During their history, a lot of great albums were released.
+These are my favorite:
+1. Abbey Road
+2. Rubber Soul
+3. Revolver
+4. A Hard Day's Night""")
