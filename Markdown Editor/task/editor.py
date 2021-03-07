6868638f8f5611f6d5381f3b8429ebc123dba1be
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-07 16:30:17 +0530

    Markdown using python - options 3/5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,13 +1,69 @@
-print("$ python markdown-editor.py")
-formats = ['plain', 'bold', 'italic', 'link', 'inline-code', 'header', 'ordered-list', 'unordered-list', 'line-break']
-while 1:
-    choice = input("- Choose a formatter: ")
-    if choice == "!help":
-        print("""Available formatters: plain bold italic link inline-code header ordered-list unordered-list line-break
-Special commands: !help !done""")
-    elif choice in formats:
-        pass
-    elif choice == "!done":
-        break
-    else:
-        print("Unknown formatting type or command. Please try again")
+def header():
+    level = int(input("- Level: "))
+    if not 0 < level < 7:
+        return 1
+    text = input("- Text: ")
+    return "#" * level + " " + text + "\n"
+
+
+def plain():
+    text = input("- Text: ")
+    return text
+
+
+def line_break():
+    return "\n"
+
+
+def bold():
+    return "**" + input("- Text: ") + "**"
+
+
+def italic():
+    return "*" + input("- Text: ") + "*"
+
+
+def inline():
+    return "`" + input("- Text: ") + "`"
+
+
+def link():
+    return "[" + input("- Label: ") + "]" + "(" + input("- URL: ") + ")"
+
+
+def main():
+    print("$ python markdown-editor.py")
+    # formats = ['plain', 'bold', 'italic', 'link', 'inline-code', 'header', 'ordered-list', 'unordered-list', 'line-break']
+    markdown = ""
+    while 1:
+        choice = input("- Choose a formatter: ")
+        if choice == "!help":
+            print("""Available formatters: plain bold italic link inline-code header ordered-list unordered-list line-break
+    Special commands: !help !done""")
+        elif choice == "!done":
+            break
+        elif choice == "plain":
+            markdown += plain()
+        elif choice == "bold":
+            markdown += bold()
+        elif choice == "italic":
+            markdown += italic()
+        elif choice == "inline-code":
+            markdown += inline()
+        elif choice == "link":
+            markdown += link()
+        elif choice == "header":
+            if header == 1:
+                print("The level should be within the range of 1 to 6")
+                continue
+            markdown += header()
+        elif choice == "line-break":
+            markdown += line_break()
+        else:
+            print("Unknown formatting type or command. Please try again")
+            continue
+        print(markdown)
+
+
+if __name__ == '__main__':
+    main()
