112e424e993d2586794d614021d6b0a0ff937274
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-09 01:09:20 +0530

    Markdown using python - ordered and unordered list 4/5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -41,7 +41,7 @@ def md_list(order):
     out = ""
     if order == 0:
         for i in range(1, rows + 1):
-            out += f"{i}." + input(f"- Row #{i}: ") + '\n'
+            out += f"{i}. " + input(f"- Row #{i}: ") + '\n'
     else:
         for i in range(1, rows + 1):
             out += "* " + input(f"- Row #{i}: ") + "\n"
