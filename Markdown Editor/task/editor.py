af0f296abd7ca8fe00e695e29c34969ef17a3aff
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-09 01:14:12 +0530

    Markdown using python - Save the results 5/5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -50,7 +50,7 @@ def md_list(order):
 
 def main():
     print("$ python markdown-editor.py")
-    # formats = ['plain', 'bold', 'italic', 'link', 'inline-code', 'header', 'ordered-list', 'unordered-list', 'line-break']
+    markdown_file = open('output.md', 'a')
     markdown = ""
     while 1:
         choice = input("- Choose a formatter: ")
@@ -84,6 +84,8 @@ def main():
             print("Unknown formatting type or command. Please try again")
             continue
         print(markdown)
+        markdown_file.write(markdown)
+    markdown_file.close()
 
 
 if __name__ == '__main__':
